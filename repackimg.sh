7b37c20b6f6fa00c1a014bcb02fe6edda8780b58
Yeliqin666 <63091085+Yeliqin666@users.noreply.github.com>
2022-07-28 11:22:40 +0800

    fix new-boot failing to boot
    你是一个一个一个

--- repackimg.sh
+++ repackimg.sh
@@ -71,7 +71,7 @@ main() {
     cd "${dirpath}/ramdisk"
 	# prefix some issues
 	#if [ -d "./ramdisk/.backup" ]; then chmod -R 0755 "./ramdisk/.backup";fi
-	sudo find | sed 1d | sudo "${cpio}" -o > "${dirpath}/split_img/ramdisk.cpio"
+	sudo find | sed 1d | sudo "${cpio}" -H newc -R 0:0 -o -F ${dirpath}/split_img/ramdisk.cpio
 	cd "${dirpath}"
   if [ "$vendorboot" = "1" ]; then
     if [ -f 'ramdisk_compress_type' ]; then
